35c18058ae693c3d3b62a01a7060f8c02cc9cfb2
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-05-18 17:04:35 +0100

    Amended css enqueue order

--- wp-content/themes/intranet-jo/lib/assets.php
+++ wp-content/themes/intranet-jo/lib/assets.php
@@ -7,9 +7,10 @@ function assets() {
   if ( is_page(5)) {
   wp_enqueue_style('styles-home', trailingslashit(get_template_directory_uri()) . "styles-home.css", false, null);
 } else {
-  wp_enqueue_style('grid', trailingslashit(get_template_directory_uri()) . "grid.css", false, null);
   wp_enqueue_style('added', trailingslashit(get_template_directory_uri()) . "added.css", false, null);
-  wp_enqueue_style('print', trailingslashit(get_template_directory_uri()) . "print.css", false, null);
+  wp_enqueue_style('grid', trailingslashit(get_template_directory_uri()) . "grid.css", false, null);
+
+
 }
  wp_enqueue_script( 'jquery-1.4', get_template_directory_uri() . '/scripts/jquery-1.4.js', false, null );
  wp_enqueue_script( 'easySlider1-7', get_template_directory_uri() . '/scripts/easySlider1-7.js', false, null );
@@ -20,6 +21,7 @@ add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
 
 function add_head() {
   echo '<!-- Styles -->
+<link href="print.css" rel="stylesheet" type="text/css" media="print" />
 
 <!--[if IE 7]>
     <link rel="stylesheet" type="text/css" href="ie7.css">
