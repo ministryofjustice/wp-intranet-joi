4ae9319ebaef2c7284aed495cb86500d55e04596
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-05-22 14:26:37 +0100

    Enqueued scripts and styles

--- wp-content/themes/intranet-jo/lib/assets.php
+++ wp-content/themes/intranet-jo/lib/assets.php
@@ -4,14 +4,16 @@ namespace Roots\Sage\Assets;
 
 function assets() {
   global $wp_styles;
+
   if ( is_page(5)) {
   wp_enqueue_style('styles-home', trailingslashit(get_template_directory_uri()) . "styles-home.css", false, null);
 } else {
   wp_enqueue_style('added', trailingslashit(get_template_directory_uri()) . "added.css", false, null);
   wp_enqueue_style('grid', trailingslashit(get_template_directory_uri()) . "grid.css", false, null);
-
-
 }
+  wp_enqueue_style( 'ie7', trailingslashit(get_template_directory_uri()) . "ie7.css", false, null  );
+  $wp_styles->add_data( 'ie7', 'conditional', 'IE 7' );
+  wp_enqueue_style( 'print', trailingslashit(get_template_directory_uri()) . "print.css", false, null, 'print'  );
  wp_enqueue_script( 'jquery-1.4', get_template_directory_uri() . '/scripts/jquery-1.4.js', false, null );
  wp_enqueue_script( 'easySlider1-7', get_template_directory_uri() . '/scripts/easySlider1-7.js', false, null );
  wp_enqueue_script( 'searchbox', get_template_directory_uri() . '/scripts/searchbox.js', false, null );
@@ -20,13 +22,7 @@ function assets() {
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
 
 function add_head() {
-  echo '<!-- Styles -->
-<link href="print.css" rel="stylesheet" type="text/css" media="print" />
-
-<!--[if IE 7]>
-    <link rel="stylesheet" type="text/css" href="ie7.css">
-<![endif]-->
-<!-- Styles end -->
+  echo '
 <!-- Scripts -->
 <script type="text/javascript">
     $(document).ready(function(){   
