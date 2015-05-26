ab3fad0a374d23a0241e4d8aa197b980727a7dce
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-05-26 09:53:38 +0100

    Enabled posts in dashboard

--- wp-content/themes/intranet-jo/lib/extras.php
+++ wp-content/themes/intranet-jo/lib/extras.php
@@ -29,7 +29,7 @@ add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
 
 function remove_menus(){
   //remove_menu_page( 'index.php' );                  //Dashboard
-  remove_menu_page( 'edit.php' );                   //Posts
+  //remove_menu_page( 'edit.php' );                   //Posts
   //remove_menu_page( 'upload.php' );                 //Media
   //remove_menu_page( 'edit.php?post_type=page' );    //Pages
   remove_menu_page( 'edit-comments.php' );          //Comments
