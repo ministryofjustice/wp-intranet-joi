9cb09bcc2bcf1cf46fa3907a1a621e701e6b9e0b
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-05-28 13:53:14 +0100

    Added query for news stories on homepage, with ACF for external links or docs

--- wp-content/themes/intranet-jo/page-home.php
+++ wp-content/themes/intranet-jo/page-home.php
@@ -89,54 +89,41 @@
 <!-- ***********  News Area *********** -->
 <div id="divNews" style="position: relative;">
 <h1>What's New</h1>
-<h2><a href="news/2015/marathon.htm">Michael Stewart Runs the Marathon</a></h2>
-<p class="date">28 April 2015</p>
-<p>Congratulations to Michael Stewart from the Communications Team, who ran the London Marathon on Sunday.</p>
-<h2><a href="working_in/finance.htm">Changes to Invoices</a></h2>
-<p class="date">28 April 2015</p>
-Please note there is now a different address
-for suppliers to send invoices to.
-<h2><a href="news/2015/new-international-team.htm"><strong>New   International Team</strong></a></h2>
-<p class="date">23 April 2015</p>
-<p>The Judicial Office has   restructured the way it delivers its international support to a variety of   senior judges. </p>
-<h2><a href="news/2015/eu-presidency.htm"><strong>Preparation for EU Presidency: Staff Skills   Survey</strong></a></h2>
-<p class="date">23 April 2015</p>
-<p>Complete the survey to register your interest in being involved in the preparation for EU Presidency in 2017.</p>
-<h2><a href="news/2015/lcj-court-usher-tribute.htm">Judges Bid Farewell to Top Usher</a></h2>
-<p class="date">21 April 2015</p>
-<p>The Lord Chief Justice was joined by three former chief justices and other top judges in a unique tribute   to court usher Jean Fairs.</p>
-<h2 sizset="65" sizcache="9"><a href="docs/agenda-jomb-28-april-2015.pdf">Judicial Office Management Board   draft agenda</a></h2>
-<p class="date" sizset="65" sizcache="9">21 April 2015</p>
-<p>Meeting on 28 April 2015</p>
-<h2><a href="http://intranet.justice.gsi.gov.uk/news-features/corporate-updates/attendance-management-changes.htm">MoJ Corporate Update - New Attendance Management Policy</a></h2>
-<p class="date">21 April 2015</p>
-<h2 sizcache013395280703002998="5" sizset="20"><a href="news/2015/esta-dis.htm">Supporting disabled staff in MoJ: Esta&rsquo;s story</a></h2>
-<p class="date" sizcache013395280703002998="5" sizset="20">21 April 2015</p>
-<p sizcache013395280703002998="5" sizset="20">Esta Rooney is the Chair of the MoJ Disability Network, read her story.</p>
-<h2 sizcache013395280703002998="5" sizset="20"><a href="news/2015/l-d-bite-size-csl.htm">'Bite-size' Learning Programme - CSL Awareness</a></h2>
-<p class="date" sizcache013395280703002998="5" sizset="20">17 April 2015</p>
-<p sizcache013395280703002998="5" sizset="20">Last spaces remaining for  1 hour 'bite-size' session on getting the most out of the the Civil Service Learning portal on 22 April.</p>
-<h2 sizcache013395280703002998="5" sizset="20"><a href="news/2015/staff-expenses-problems-bec.htm"><strong>Staff Expenses &ndash; problems with Business Entity Codes</strong></a></h2>
-<p class="date" sizcache013395280703002998="5" sizset="20">17 April 2015</p>
-<p sizcache013395280703002998="5" sizset="20">Issues with  Business  Entity Code record in the re-configured iExpenses system on DOM1.</p>
-<h2 sizcache013395280703002998="5" sizset="20"><a href="http://intranet.justice.gsi.gov.uk/news-features/calendar/11207.htm">Lunch &amp; Learn: Criminal Cases Review Commission</a></h2>
-<p class="date" sizcache013395280703002998="5" sizset="20">16 April 2015</p>
-<p sizcache013395280703002998="5" sizset="20">Discussion on the role of the CCRC on  22 April 2015. Book now.</p>
-<h2 sizcache013395280703002998="5" sizset="20"><a href="news/2015/april-sums.htm">April Stand Up Meetings - Updated</a></h2>
-<p class="date" sizcache013395280703002998="5" sizset="20">16 April 2015</p>
-<p sizcache013395280703002998="5" sizset="20">Update: Judicial College SUM rescheduled to Wednesday  3:30pm.</p>
-<h2 sizcache013395280703002998="5" sizset="20"><a href="http://intranet.justice.gsi.gov.uk/news-features/corporate-updates/merge-15.htm">MoJ Corporate Update - Phoenix open for financial and expense claims</a></h2>
-<p class="date" sizcache013395280703002998="5" sizset="20">16 April 2015</p>
-<h2 sizcache013395280703002998="5" sizset="20"><a href="news/2015/e-learning-expenses.htm"><strong>Staff Expenses &ndash; claiming and approving</strong></a></h2>
-<p class="date" sizcache013395280703002998="5" sizset="20">16 April 2015</p>
-<p sizcache013395280703002998="5" sizset="20">eLearning for  staff expenses is available on the Justice Academy.</p>
-<h2 sizcache013395280703002998="5" sizset="20"><a href="news/2015/new-finance-forms.htm">New Finance Forms</a></h2>
-<p class="date" sizcache013395280703002998="5" sizset="20">15 April 2015</p>
-<p sizcache013395280703002998="5" sizset="20">New forms for use from 1 April 2015 are now available on the MOJ intranet - discard any forms used previously.</p>
-<h2 sizcache013395280703002998="5" sizset="20"><a href="news/2015/bitesize-learning-wwj.htm">'Bite-size' Learning Programme - Working with Judges</a></h2>
-<p class="date" sizcache013395280703002998="5" sizset="20">10 April 2015</p>
-<p sizcache013395280703002998="5" sizset="20">Spaces are available for the latest series of seminars. In this session District Judge  Arbuthnot will be talking about  her role.</p>
-<h2 sizcache013395280703002998="5" sizset="20">&nbsp;</h2>
+
+
+<?php
+	// Get meta value containing array of entries
+	$latest_news_args = array(
+		'post_type' => 'post',
+		'posts_per_page' => 30
+	);
+	$latest_news_query = new WP_Query( $latest_news_args );
+	// Iterate over entries and display
+	while ( $latest_news_query->have_posts() ) : $latest_news_query->the_post();
+		?>
+		<h2><a href="
+		<?php if (get_field('external_file_or_link'))
+		{
+		 echo get_field('external_file_or_link'); 
+		}
+		else {
+		 echo the_permalink();
+		}
+		?>
+		"><?php the_title(); ?></a></h2>
+
+			<p class="date">
+			 <time class="published" datetime="<?php echo get_the_time( 'c' ); ?>"><?php echo get_the_date(); ?></time>
+			</p>
+			<?php the_excerpt(); ?>
+
+
+			<?php endwhile; ?>
+		<?php wp_reset_postdata(); ?>
+
+<!-- <h2><a href="news/2015/marathon.htm">Michael Stewart Runs the Marathon</a></h2>
+<p class="date">28 April 2015</p> -->
+
 <h2><a href="archived-news.htm"><img src="wp-content/uploads/news-archive.png" width="180" height="30" border="0" alt="News archive image" /></a></h2>
 </div>
 <!-- ***********  End of News Area *********** -->
