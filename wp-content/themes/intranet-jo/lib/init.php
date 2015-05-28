2e357a497d6ca97619570c6235b9e47344db0d9a
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-05-28 09:33:12 +0100

    Small adjustment so code will run on WPEngine's older php

--- wp-content/themes/intranet-jo/lib/init.php
+++ wp-content/themes/intranet-jo/lib/init.php
@@ -32,21 +32,21 @@ add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
  * Register sidebars
  */
 function widgets_init() {
-  register_sidebar([
+  register_sidebar(array(
     'name'          => __('Primary', 'sage'),
     'id'            => 'sidebar-primary',
     'before_widget' => '<section class="widget %1$s %2$s">',
     'after_widget'  => '</section>',
     'before_title'  => '<h3>',
     'after_title'   => '</h3>'
-  ]);
-  register_sidebar([
+  );
+  register_sidebar(array(
     'name'          => __('Footer', 'sage'),
     'id'            => 'sidebar-footer',
     'before_widget' => '<section class="widget %1$s %2$s">',
     'after_widget'  => '</section>',
     'before_title'  => '<h3>',
     'after_title'   => '</h3>'
-  ]);
+  );
 }
 add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
