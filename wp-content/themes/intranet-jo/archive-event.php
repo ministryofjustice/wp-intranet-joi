37e5266c591c973e1693f87c475617185e9399c1
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-05-28 09:31:56 +0100

    Calendar layout

--- wp-content/themes/intranet-jo/archive-event.php
+++ wp-content/themes/intranet-jo/archive-event.php
@@ -2,6 +2,20 @@
 
 <?php get_template_part('templates/page', 'header'); ?>
 
+<div class="row" id="main">
+
+<!-- Left nav -->
+<div class="column grid_3">
+<?php wp_nav_menu( array('menu' => 'Events' )); ?>                                            
+</div>
+     <!-- Left nav end -->          
+     <!-- Mid section -->
+     <div class="column grid_12" id="mid">
+<h1>Events calendar </h1>
+<h2><? echo date('F Y'); ?></h2>
+
+<div class="box">
+
 <?php $query = Extras\event_query(); ?>
 
 <?php if (!$query->have_posts()) : ?>
@@ -10,7 +24,7 @@
   </div>
   <?php get_search_form(); ?>
 <?php endif; ?>
-
+<?php $dategroup = null; ?>
 <?php while ($query->have_posts()) : $query->the_post(); ?>
   <?php $postdate = get_field('date'); $date = DateTime::createFromFormat('d/m/Y', $postdate); ?>
   <?php if ( $postdate != $dategroup ): ?>
@@ -26,3 +40,10 @@
 <?php endif; ?>
 
 <?php the_posts_navigation(); ?>
+
+</div>
+
+</div>
+
+</div>
+
