cf3a92bb9ae255f26722b4d7fd57c0a1156e99cd
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-06-05 15:32:13 +0100

    Date on blog posts

--- wp-content/themes/intranet-jo/templates/content-single.php
+++ wp-content/themes/intranet-jo/templates/content-single.php
@@ -2,6 +2,7 @@
   <article <?php post_class(); ?>>
     <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
+      <p><strong><?php echo get_the_date('m F Y'); ?></strong></p>
     </header>
     <div class="entry-content">
       <?php the_content(); ?>
