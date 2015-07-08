bcecaa27b0883207c465e1444e57963e0788de3e
Toby Schrapel <toby@tobyschrapel.com>
2015-07-08 10:41:32 +0100

    Search box

--- wp-content/themes/jo/templates/searchform.php
+++ wp-content/themes/jo/templates/searchform.php
@@ -1,7 +1,7 @@
 <div class="search-top" id="int">
   <form action="<?= esc_url(home_url('/')); ?>" method="get" name="query" id="query">
     <label for="search" class="hidden-text">Search:</label>
-    <input id="search" name="s" class="searchbox" size="30" type="text">
+    <input id="search" name="s" class="searchbox" size="25" type="text">
     <input id="submit" value="Go" class="searchbutton" title="Search" type="submit"><br><br>
     Search:
     <input name="searchtype" id="intranet" value="intranet" checked="checked" type="radio">
@@ -13,9 +13,9 @@
 <div class="search-people" id="pf">
   <form action="http://intranet-applications.dca.gsi.gov.uk/peopleFinder/UserSearch2.do" method="get" name="searchform" id="searchform">
     <label for="forename" class="hidden-text">Forename</label>
-    <input id="forename" name="forename" type="text" class="searchbox" size="12" value="First name" onclick="this.value = '';" />
+    <input id="forename" name="forename" type="text" class="searchbox" size="10" value="First name" onclick="this.value = '';" />
     <label for="surname" class="hidden-text">Surname</label>
-    <input id="surname" name="surname" type="text" class="searchbox" size="13" value="Last name" onclick="this.value = '';" />
+    <input id="surname" name="surname" type="text" class="searchbox" size="10" value="Last name" onclick="this.value = '';" />
     <input type="submit" class="searchbutton" id="submit1" title="Search" value="Go" />
     <br>Search:
     <input name="searchtype1" type="radio" id="intranet1" value="intranet" />
