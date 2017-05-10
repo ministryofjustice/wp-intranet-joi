5c569d084a8601f3e171a404a83da837ecf4eb8b
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-05-10 11:03:45 +0100

    Remove hardcoded Google Analytics tracking in favour of plugin Google Analytics Dashboard for WP.

--- web/app/themes/jo/templates/footer.php
+++ web/app/themes/jo/templates/footer.php
@@ -7,13 +7,3 @@
 Content owner: <a href="mailto:website.enquiries@judiciary.gsi.gov.uk?subject=JO intranet content"><!-- InstanceBeginEditable name="owner" -->Judicial Office Communications Team<!-- InstanceEndEditable --></a><br />Last updated: <!-- #BeginDate format:En2 --><?= get_the_modified_date( 'd F Y' ); ?><!-- #EndDate -->.
 </div>
 </div>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-1165903-11', 'auto');
-  ga('send', 'pageview');
-
-</script>
